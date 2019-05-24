282f5d665233750d832fdec0a187c88fe8527ab1
kevinxiao1 <40753620+kevinxiao1@users.noreply.github.com>
2019-05-24 16:11:45 +0700

    Update cekMhsAbsen.php

--- cekMhsAbsen.php
+++ cekMhsAbsen.php
@@ -3,7 +3,7 @@
 include "function.php";
 $timestamp = date("Y-m-d");
 if ($_SESSION['idMahasiswa'] == $_POST['nrp']) {
-    executeNonQuery($mysqli,"insert into absen_mhs (nrp_mhs, id_kelas ,tanggal_absen) values('".$_POST['nrp']."','".$_SESSION['idKelas']."','$timestamp')");
+    executeNonQuery($mysqli,"insert into absen_mhs(nrp_mhs, id_kelas ,tanggal_absen) values('".$_POST['nrp']."','".$_SESSION['idKelas']."','$timestamp')");
     echo "sukses";
 }else{
     echo "gagal";
